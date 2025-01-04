cebc48eec32e5992b3ff85805a74d0977fa33b79
Luciano <70247350+Lucho-Vidal@users.noreply.github.com>
2025-01-03 21:04:03 -0300

    add server

---
+++ server/src/main/java/com/kioskoApp/luvidev_leoHaron/LuvidevNahuelHaronApplication.java
@@ -0,0 +1,13 @@
+package com.kioskoApp.luvidev_leoHaron;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuvidevNahuelHaronApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuvidevNahuelHaronApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/com/kioskoApp/luvidev_leoHaron/LuvidevNahuelHaronApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kioskoApp.luvidev_leoHaron;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvidevNahuelHaronApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
